766ba9d95b1827f7dd0021149247b5a77bc3ecda
Bryan Diaz <vicentepomas.2007@gmail.com>
2025-01-08 12:42:54 -0500

    cambios

--- ejemplos/Ejemplos/src/ejemplos03/Ejemplo08.java
+++ ejemplos/Ejemplos/src/ejemplos03/Ejemplo08.java
@@ -5,7 +5,6 @@
  */
 package ejemplos03;
 
-import java.awt.BorderLayout;
 import java.util.Scanner;
 
 /**
@@ -15,61 +14,59 @@ import java.util.Scanner;
 public class Ejemplo08 {
 
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-
+        Scanner entrada= new Scanner (System.in);
         int limite;
         int tabla;
-        int signo;
-        String mensajeSuma1;
-        String mensajeSuma2;
-
-        System.out.println("Ingrese limite de la tabla");
+        int opcion;
+        String mensajeSuma;
+        String mensajeSumaDos;
+        
+        System.out.printf("Ingrese el limite de la tabla:", args); 
         limite = entrada.nextInt();
-        System.out.println("Ingrese la tabla a realizar");
+        
+        System.out.printf("Ingrese la tabla que desea realizar:", args);
         tabla = entrada.nextInt();
-        System.out.println("Ingrese apoeracion a realizar suma(1) o"
-                + "multiplicacion (2)");
-        signo = entrada.nextInt();
-
-        switch (signo) {
+        
+        System.out.printf("Ingrese la operacion que desea realizar (1)suma,"
+                + "(2)multiplicación:", args);
+        opcion = entrada.nextInt();
+        
+        switch(opcion){
             case 1:
-                
-                mensajeSuma1 = obtenerTablaSumar(limite, tabla);
-                System.out.println(mensajeSuma1);
-                
-            break;
-            
+                mensajeSuma= obtenerTablaSumar(limite, tabla);
+                System.out.printf("%s\n", mensajeSuma);
+                break;
             case 2:
-                mensajeSuma2 = obtenerTablaMultiplicar(limite, tabla);
-                System.out.println(mensajeSuma2);
-                
-            break;
+                mensajeSumaDos = obtenerTablaMultiplicar(limite, tabla);
+                System.out.printf("%s\n", mensajeSumaDos);
+                break;
         }
-
+        
+       
+       
     }
-
-    public static String obtenerTablaSumar(int limite, int tabla) {
+        
+    public static String obtenerTablaSumar(int limite, int tabla){
+        int operacion;
+        String cadena = "";
+        for (int i = 1; i <= limite; i++) {
+            operacion = tabla + i;
+            cadena = String.format("%s%d + %d = %d\n", cadena, tabla, i, 
+                    operacion);
+        }
+        return cadena;        
+    }
+    
+    public static String obtenerTablaMultiplicar(int limite, int tabla){
         int operacion;
         String cadena = "";
         for (int i = 1; i <= limite; i++) {
             operacion = tabla * i;
-            cadena = String.format("%s%d * %d = %d\n", cadena, tabla, i,
+            cadena = String.format("%s%d * %d = %d\n", cadena, tabla, i, 
                     operacion);
         }
         return cadena;
-
+        
     }
-
-    public static String obtenerTablaMultiplicar(int limite, int tabla) {
-        int operacion;
-        String cadena = "";
-        for (int i = 1; i <= limite; i++) {
-            operacion = tabla * i;
-            cadena = String.format("%s%d * %d = %d\n", cadena, tabla, i,
-                    operacion);
-        }
-        return cadena;
-
-    }
-
+    
 }
